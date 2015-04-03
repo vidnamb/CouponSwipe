2bd2f25175fbb223aed3e240ec53e885b15542cb
Vidya <vnambiar@andrew.cmu.edu>
2015-04-03 12:41:24 -0700

    Initial commit

---
+++ app/src/main/java/couponswipe/cmu/edu/couponswipe/ui/DealStackActivity.java
@@ -0,0 +1,46 @@
+package couponswipe.cmu.edu.couponswipe.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import couponswipe.cmu.edu.couponswipe.R;
+
+/**
+ * MainActivity - renamed to DealStackActivity
+ * activity_main.xml renamed to activity_deal_stack
+ *
+ */
+
+public class DealStackActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deal_stack);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
