ebd1dd4a1f52cb584f6eddd52bfe522c5448ddd7
Vidya <vnambiar@andrew.cmu.edu>
2015-04-04 21:46:35 -0700

    Deal Stack UI

--- app/src/main/java/couponswipe/cmu/edu/couponswipe/ui/DealStackActivity.java
+++ app/src/main/java/couponswipe/cmu/edu/couponswipe/ui/DealStackActivity.java
@@ -1,9 +1,13 @@
 package couponswipe.cmu.edu.couponswipe.ui;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import couponswipe.cmu.edu.couponswipe.R;
 
@@ -15,10 +19,14 @@ import couponswipe.cmu.edu.couponswipe.R;
 
 public class DealStackActivity extends Activity {
 
+    private int currentDemoDeal = 0;    //0,1,2 represent three sample deals
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deal_stack);
+
+
     }
 
 
@@ -43,4 +51,72 @@ public class DealStackActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void likeDeal(View view){
+
+        showNextDealInStack();
+    }
+
+    public void discardDeal(View view){
+
+        showNextDealInStack();
+    }
+
+    public void showNextDealInStack(){
+
+        currentDemoDeal++;
+        if(currentDemoDeal>2)
+            currentDemoDeal=0;
+
+        ImageView dealImageView = (ImageView) findViewById(R.id.coupon_imageView);
+        TextView dealTitleTextView = (TextView) findViewById(R.id.deal_title_textView);
+        TextView dealStoreTextView = (TextView) findViewById(R.id.deal_store_textView);
+        TextView dealDistanceTextView = (TextView) findViewById(R.id.deal_distance_textView);
+        TextView dealPriceTextView = (TextView) findViewById(R.id.deal_price_textView);
+
+        switch (currentDemoDeal)
+        {
+            case 0:
+            {
+                dealImageView.setImageResource(R.drawable.easter_coupon);
+                dealTitleTextView.setText(getString(R.string.demo_coupon_title_1));
+                dealStoreTextView.setText(getString(R.string.demo_coupon_store_1));
+                dealDistanceTextView.setText(getString(R.string.demo_coupon_distance_1));
+                dealPriceTextView.setText(getString(R.string.demo_coupon_price_1));
+                break;
+            }
+            case 1:
+            {
+                dealImageView.setImageResource(R.drawable.omaha_steak);
+                dealTitleTextView.setText(getString(R.string.demo_coupon_title_2));
+                dealStoreTextView.setText(getString(R.string.demo_coupon_store_2));
+                dealDistanceTextView.setText(getString(R.string.demo_coupon_distance_2));
+                dealPriceTextView.setText(getString(R.string.demo_coupon_price_2));
+                break;
+            }
+            case 2:
+            {
+                dealImageView.setImageResource(R.drawable.golden_gate);
+                dealTitleTextView.setText(getString(R.string.demo_coupon_title_3));
+                dealStoreTextView.setText(getString(R.string.demo_coupon_store_3));
+                dealDistanceTextView.setText(getString(R.string.demo_coupon_distance_3));
+                dealPriceTextView.setText(getString(R.string.demo_coupon_price_3));
+                break;
+            }
+        }
+
+    }
+
+    public void openUserProfile(View view)
+    {
+        Intent intent = new Intent(this, UserProfileActivity.class);
+        startActivity(intent);
+    }
+
+    public void openShortlist(View view)
+    {
+        Intent intent = new Intent(this, DealShortlistActivity.class);
+        startActivity(intent);
+    }
+
 }
