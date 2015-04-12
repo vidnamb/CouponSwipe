1686876096495bb95a31c23bb47edf628cfdb365
Lloyd D'Silva <lloyddsilva@gmail.com>
2015-04-11 21:56:44 -0700

    Added first cut of deal service

--- app/src/main/java/edu/cmu/couponswipe/ws/local/GrouponDealService.java
+++ app/src/main/java/edu/cmu/couponswipe/ws/local/GrouponDealService.java
@@ -1,7 +1,51 @@
 package edu.cmu.couponswipe.ws.local;
 
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import edu.cmu.couponswipe.ws.remote.ICouponswipeService;
+
 /**
  * Created by lloyddsilva on 11/4/15.
  */
-public class GrouponDealService {
+public class GrouponDealService extends Service {
+    private static final String TAG = "GrouponDealService";
+
+    public class GrouponDealServiceImpl extends IDealService.Stub
+    {
+//        @Override
+//        public User authenticateUser(String ticker) throws RemoteException
+//        {
+//            Log.v(TAG, "getQuote() called for " + ticker);
+//            return new User();
+//        }
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.v(TAG, "onCreate() called");
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+        Log.v(TAG, "onDestroy() called");
+    }
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+        super.onStart(intent, startId);
+        Log.v(TAG, "onStart() called");
+    }
+
+    @Override
+    public IBinder onBind(Intent intent)
+    {
+        Log.v(TAG, "onBind() called");
+        return new GrouponDealServiceImpl();
+    }
 }
