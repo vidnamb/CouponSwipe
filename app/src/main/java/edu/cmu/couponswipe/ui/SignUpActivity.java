4899bfd72f3568738c39d77e5d4903985f565a52
sparshithp <sparsh.pro@gmail.com>
2015-04-30 21:32:03 -0700

    "Signup implemented"

--- app/src/main/java/edu/cmu/couponswipe/ui/SignUpActivity.java
+++ app/src/main/java/edu/cmu/couponswipe/ui/SignUpActivity.java
@@ -4,14 +4,58 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import org.apache.http.HeaderElement;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
 
 import edu.cmu.couponswipe.R;
 import edu.cmu.couponswipe.ui.intents.Intents;
 
 public class SignUpActivity extends Activity {
+
+
+    EditText firstNameET;
+    EditText lastNameET;
+    EditText emailET;
+    EditText phoneNumberET;
+    EditText passwordET;
+
+    String fName;
+    String lName;
+    String email;
+    String phone;
+    String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +64,13 @@ public class SignUpActivity extends Activity {
 
         ActionBar actionBar = getActionBar();
         actionBar.hide();
+
+        firstNameET = (EditText)findViewById(R.id.firstNameEditText);
+        lastNameET = (EditText)findViewById(R.id.lastNameEditText);
+        emailET = (EditText)findViewById(R.id.userEmailEditText);
+        phoneNumberET = (EditText)findViewById(R.id.userPhoneEditText);
+        passwordET = (EditText)findViewById(R.id.userPasswordEditText);
+
     }
 
 
@@ -46,10 +97,64 @@ public class SignUpActivity extends Activity {
     }
 
     public void signup(View view){
-        Intents.openDealStack(this);
+
+        fName = firstNameET.getText().toString();
+        lName = lastNameET.getText().toString();
+        email = emailET.getText().toString();
+        phone = phoneNumberET.getText().toString();
+        password = passwordET.getText().toString();
+        JSONObject obj = new JSONObject();
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+        StrictMode.setThreadPolicy(policy);
+        try{
+            obj.put("email",email);
+            obj.put("firstName", fName);
+            obj.put("lastName", lName);
+            obj.put("password", password);
+            obj.put("phoneNumber", phone);
+            obj.put("prefCategories", "");
+            obj.put("prefDistance", 10);
+
+            String user = obj.toString();
+            DefaultHttpClient httpclient = new DefaultHttpClient();
+            HttpPost httpost = new HttpPost("http://10.0.3.2:8080/user/signup");
+            StringEntity se = new StringEntity(user);
+
+            httpost.setEntity(se);
+            httpost.setHeader("Accept", "application/json");
+            httpost.setHeader("Content-type", "application/json");
+
+            ResponseHandler responseHandler = new BasicResponseHandler();
+            String response = (String) httpclient.execute(httpost, responseHandler);
+System.out.println();
+            Intents.openDealStack(this);
+        }catch(JSONException e){
+            Toast.makeText(getApplicationContext(), "JSON", Toast.LENGTH_LONG).show();
+
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            Toast.makeText(getApplicationContext(), "UserName already taken", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            System.out.println("#######");
+
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("%%%%%");
+            Toast.makeText(getApplicationContext(), "Problem with the connection the server", Toast.LENGTH_LONG).show();
+
+            e.printStackTrace();
+        }
     }
 
     public void cancel(View view) {
         Intents.openLoginPage(this);
     }
+
+
+
+
+
+
 }
